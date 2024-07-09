394ee1c8af8b9fc6757ae5cc9ef13f5b5afcdaf9
sheriff786 <mohdsheriff27021996@gmail.com>
2024-07-09 19:39:41 +0530

    First whole commit

---
+++ src/helper.py
@@ -0,0 +1,50 @@
+import os
+from PyPDF2 import PdfReader
+from langchain_google_genai import GoogleGenerativeAI
+from langchain_community.llms import GooglePalm
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import GooglePalmEmbeddings
+# from langchain.llms import GooglePalm
+from langchain_community.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from dotenv import load_dotenv
+
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")  
+os.environ['GOOGLE_API_KEY'] =  GOOGLE_API_KEY
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+
+def get_vector_store(text_chunks):
+    embeddings = GooglePalmEmbeddings()
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    return vector_store
+
+
+
+def get_conversational_chain(vector_store):
+    llm=GoogleGenerativeAI(model="models/text-bison-001", google_api_key=GOOGLE_API_KEY)
+
+    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+    return conversation_chain
